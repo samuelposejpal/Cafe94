ec7cc9534f121ef975a96dcec9820fff552b40da
Numph <62542308+Numph@users.noreply.github.com>
2020-04-16 22:45:10 +0100

    Update thankyouBookingController.java

--- NEWnewestNEWcode/thankyouBookingController.java
+++ NEWnewestNEWcode/thankyouBookingController.java
@@ -10,6 +10,10 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 import java.io.IOException;
 
+/**
+ * Controller class for scene that is generated once a booking has been placed.
+ * @author Niamh Murphy-Mauchline
+ */
 
 public class thankyouBookingController {
 
@@ -18,6 +22,9 @@ public class thankyouBookingController {
         @FXML
         private Button goBackThanks;
 
+        /**
+        * Returns to the login scene when button is pushed.
+        */
         @FXML
         public void goHomeTY(ActionEvent event) throws IOException {
             Parent thanksToHome = FXMLLoader.load(getClass().getResource("loginimproved.fxml"));
@@ -27,7 +34,10 @@ public class thankyouBookingController {
             window.setScene(thanksToHomeScene);
             window.show();
         }
-
+        
+        /**
+        * Returns the user back one scene to the booking request page.
+        */
         @FXML
         public void goBackBookRequest(ActionEvent event) throws IOException {
             Parent backBookRequest = FXMLLoader.load(getClass().getResource("bookingRequest.fxml"));
